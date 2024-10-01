d5035345a31ffc9fdcd4591a9ef1db3e3997761d
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-10-01 12:18:30 +0000

    Added widget-tweaks to requirements.txt

--- top_10_anime/settings.py
+++ top_10_anime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -28,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jackh1155-top10ratedani-oehoeg68a47.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -172,4 +173,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
