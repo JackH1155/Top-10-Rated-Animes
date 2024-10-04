176bddb5834f7665537392f04215e118ef139579
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-10-04 10:39:28 +0000

    fixed some testing typos. add more testing images

--- userbio/forms.py
+++ userbio/forms.py
@@ -2,35 +2,36 @@ from django import forms
 from allauth.account.forms import SignupForm
 from .models import UserBio
 
+
 class CustomSignupForm(SignupForm):
     date_of_birth = forms.DateField(
-        required=False, 
+        required=False,
         widget=forms.SelectDateWidget(
-            years=range(1900, 2025), 
+            years=range(1900, 2025),
             attrs={'class': 'form-control dob', 'placeholder': 'Select Date of Birth'}
         )
     )
-    
+
     country = forms.CharField(
-        required=False, 
+        required=False,
         widget=forms.TextInput(
             attrs={'class': 'form-control country', 'placeholder': 'Enter Country'}
         )
     )
-    
+
     fav_anime = forms.CharField(
-        max_length=200, 
-        required=False, 
+        max_length=200,
+        required=False,
         widget=forms.TextInput(
             attrs={'class': 'form-control fav_anime', 'placeholder': 'Enter Favourite Anime'}
         )
     )
-    
+
     bio = forms.CharField(
         widget=forms.Textarea(
             attrs={'class': 'form-control bio', 'rows': 4, 'placeholder': 'Tell us about yourself'}
-        ), 
-        max_length=300, 
+        ),
+        max_length=300,
         required=False
     )
 
@@ -56,16 +57,13 @@ class CustomSignupForm(SignupForm):
         return user
 
 
-from django import forms
-from .models import UserBio
-
 class UserBioForm(forms.ModelForm):
     # Customizing the ImageField widget to remove "currently" file name
     profile_picture = forms.ImageField(
         required=False,
         widget=forms.ClearableFileInput(attrs={
-            'class': 'form-control', 
-            'style': 'max-width: 250px;', 
+            'class': 'form-control',
+            'style': 'max-width: 250px;',
             'initial-text': '',  # Removes the "Currently" text
             'input-text': 'Change',  # Text for "Choose file"
             'clearable': False,  # Optional: to disable "Clear" checkbox if needed
